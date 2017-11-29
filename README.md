d6c3f6b8b40ff45f7fe690c4aa4ae1cff90b8250
cahyotabby <cahyo.04pagi@gmail.com>
2017-11-29 22:29:30 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,69 +1,30 @@
-peerflix-server
-===============
+# LineVodka
+_May the Vodka be with you..._
 
-[![NPM Version][npm-image]][npm-url]
-[![NPM Downloads][downloads-image]][downloads-url]
-[![Node.js Version][node-version-image]][node-version-url]
-[![Build Status][travis-image]][travis-url]
+Tutorial
+------
+Recomended for python 2.7
 
-<img src="https://cdn.rawgit.com/asapach/peerflix-server/master/app/images/logo.svg" alt="logo" height="256">
+Using git :
 
-Streaming torrent client for node.js with web ui.
+    $ git clone http://github.com/merkremont/LineVodka
 
-![screen capture](https://cdn.rawgit.com/asapach/peerflix-server/master/capture.gif)
+Python packages :
 
-Based on [torrent-stream](https://github.com/mafintosh/torrent-stream), inspired by [peerflix](https://github.com/mafintosh/peerflix).
+    $ pip install thrift==0.9.3
+    $ pip install rsa
+    $ pip install requests
 
-## Usage
+Run Vodka :
 
-1. `npm install -g peerflix-server`
-1. `peerflix-server`
-1. Open your browser at [http://localhost:9000/](http://localhost:9000/)
-1. Enjoy!
+    $ python vodkabot.py
 
-## Configuration
+Video Tutorial
+------
 
-You can configure the application using `~/.config/peerflix-server/config.json` file (doesn't exist by default). The [options](https://github.com/mafintosh/torrent-stream#full-api) are passed to all torrent-stream instances. Here's an example that overrides the defaults:
+[![Tutorial Build BOT LINE Unofficial Protector and Kicker Group](http://i.imgur.com/C8xYq7v.png "Tutorial Build BOT LINE Unofficial Protector and Kicker Group")](https://youtu.be/anoF3jnWl2A)
 
-```json
-{
-  "connections": 50,
-  "tmp": "/mnt/torrents"
-}
-```
+Author
+------
 
-The application stores its current state (list of torrents) in `~/.config/peerflix-server/torrents.json`
-
-## Daemon
-
-If you want to run peerflix-server as a daemon, you can do it using [forever](https://github.com/nodejitsu/forever):
-
-`npm install -g forever`
-
-`forever start $(which peerflix-server)`
-
-You might also want to enable logging -- see the [docs](https://github.com/nodejitsu/forever#using-forever-from-the-command-line).
-
-## Heroku
-[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
-
-## Development
-
-See [Development.md](Development.md)
-
-## REST API
-
-See [REST.md](REST.md)
-
-## Docker
-
-See [Docker.md](Docker.md)
-
-[npm-image]: https://img.shields.io/npm/v/peerflix-server.svg?style=flat
-[npm-url]: https://npmjs.org/package/peerflix-server
-[node-version-image]: https://img.shields.io/node/v/peerflix-server.svg?style=flat
-[node-version-url]: http://nodejs.org/download/
-[travis-image]: https://img.shields.io/travis/asapach/peerflix-server.svg?style=flat
-[travis-url]: https://travis-ci.org/asapach/peerflix-server
-[downloads-image]: https://img.shields.io/npm/dm/peerflix-server.svg?style=flat
-[downloads-url]: https://npmjs.org/package/peerflix-server
+Merk Kremont / [@merkremont](https://twitter.com/merkremont)
---
+++ vodkabot.py
@@ -0,0 +1,176 @@
+﻿# -*- coding: utf-8 -*-
+from LineAlpha import LineClient
+from LineAlpha.LineApi import LineTracer
+from LineAlpha.LineThrift.ttypes import Message
+from LineAlpha.LineThrift.TalkService import Client
+import time, datetime, random ,sys, re, string, os, json
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+client = LineClient()
+client._qrLogin("line://au/q/")
+
+profile, setting, tracer = client.getProfile(), client.getSettings(), LineTracer(client)
+offbot, messageReq, wordsArray, waitingAnswer = [], {}, {}, {}
+
+print client._loginresult()
+
+wait = {
+    'readPoint':{},
+    'readMember':{},
+    'setTime':{},
+    'ROM':{}
+   }
+
+setTime = {}
+setTime = wait["setTime"]
+
+def sendMessage(to, text, contentMetadata={}, contentType=0):
+    mes = Message()
+    mes.to, mes.from_ = to, profile.mid
+    mes.text = text
+
+    mes.contentType, mes.contentMetadata = contentType, contentMetadata
+    if to not in messageReq:
+        messageReq[to] = -1
+    messageReq[to] += 1
+    client._client.sendMessage(messageReq[to], mes)
+
+def NOTIFIED_ADD_CONTACT(op):
+    try:
+        sendMessage(op.param1, client.getContact(op.param1).displayName + "Thanks for add")
+    except Exception as e:
+        print e
+        print ("\n\nNOTIFIED_ADD_CONTACT\n\n")
+        return
+
+tracer.addOpInterrupt(5,NOTIFIED_ADD_CONTACT)
+
+def NOTIFIED_ACCEPT_GROUP_INVITATION(op):
+    #print op
+    try:
+        sendMessage(op.param1, client.getContact(op.param2).displayName + "WELCOME to " + group.name)
+    except Exception as e:
+        print e
+        print ("\n\nNOTIFIED_ACCEPT_GROUP_INVITATION\n\n")
+        return
+
+tracer.addOpInterrupt(17,NOTIFIED_ACCEPT_GROUP_INVITATION)
+
+def NOTIFIED_KICKOUT_FROM_GROUP(op):
+    try:
+        sendMessage(op.param1, client.getContact(op.param3).displayName + " Good Bye\n(*´･ω･*)")
+    except Exception as e:
+        print e
+        print ("\n\nNOTIFIED_KICKOUT_FROM_GROUP\n\n")
+        return
+
+tracer.addOpInterrupt(19,NOTIFIED_KICKOUT_FROM_GROUP)
+
+def NOTIFIED_LEAVE_GROUP(op):
+    try:
+        sendMessage(op.param1, client.getContact(op.param2).displayName + " Good Bye\n(*´･ω･*)")
+    except Exception as e:
+        print e
+        print ("\n\nNOTIFIED_LEAVE_GROUP\n\n")
+        return
+
+tracer.addOpInterrupt(15,NOTIFIED_LEAVE_GROUP)
+
+def NOTIFIED_READ_MESSAGE(op):
+    #print op
+    try:
+        if op.param1 in wait['readPoint']:
+            Name = client.getContact(op.param2).displayName
+            if Name in wait['readMember'][op.param1]:
+                pass
+            else:
+                wait['readMember'][op.param1] += "\n・" + Name
+                wait['ROM'][op.param1][op.param2] = "・" + Name
+        else:
+            pass
+    except:
+        pass
+
+tracer.addOpInterrupt(55, NOTIFIED_READ_MESSAGE)
+
+def RECEIVE_MESSAGE(op):
+    msg = op.message
+    try:
+        if msg.contentType == 0:
+            try:
+                if msg.to in wait['readPoint']:
+                    if msg.from_ in wait["ROM"][msg.to]:
+                        del wait["ROM"][msg.to][msg.from_]
+                else:
+                    pass
+            except:
+                pass
+        else:
+            pass
+    except KeyboardInterrupt:
+	       sys.exit(0)
+    except Exception as error:
+        print error
+        print ("\n\nRECEIVE_MESSAGE\n\n")
+        return
+
+tracer.addOpInterrupt(26, RECEIVE_MESSAGE)
+
+def SEND_MESSAGE(op):
+    msg = op.message
+    try:
+        if msg.toType == 0:
+            if msg.contentType == 0:
+                if msg.text == "me":
+                    sendMessage(msg.to, text=None, contentMetadata={'mid': msg.from_}, contentType=13)
+                else:
+                    pass
+            else:
+                pass
+        if msg.toType == 2:
+            if msg.contentType == 0:
+                if msg.text == "Dropbox":
+		    sendMessage("https://www.dropbox.com/scl/fo/913jnpjxbhn7ddth5zs5s")
+                if msg.text == "dropbox":
+		    sendMessage("https://www.dropbox.com/scl/fo/913jnpjxbhn7ddth5zs5s")
+                if msg.text == "set":
+                    sendMessage(msg.to, "puja kerang ajaib")
+                    try:
+                        del wait['readPoint'][msg.to]
+                        del wait['readMember'][msg.to]
+                    except:
+                        pass
+                    wait['readPoint'][msg.to] = msg.id
+                    wait['readMember'][msg.to] = ""
+                    wait['setTime'][msg.to] = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+                    wait['ROM'][msg.to] = {}
+                    print wait
+                if msg.text == "sider":
+                    if msg.to in wait['readPoint']:
+                        if wait["ROM"][msg.to].items() == []:
+                            chiya = ""
+                        else:
+                            chiya = ""
+                            for rom in wait["ROM"][msg.to].items():
+                                print rom
+                                chiya += rom[1] + "\n"
+
+                        sendMessage(msg.to, "People who readed %s\nthat's it\n\nPeople who have ignored reads\n%sIt is abnormal ♪\n\nReading point creation date n time:\n[%s]"  % (wait['readMember'][msg.to],chiya,setTime[msg.to]))
+                    else:
+                        sendMessage(msg.to, "An already read point has not been set.\n「set」you can send ♪ read point will be created ♪")
+                else:
+                    pass
+        else:
+            pass
+
+    except Exception as e:
+        print e
+        print ("\n\nSEND_MESSAGE\n\n")
+        return
+
+tracer.addOpInterrupt(25,SEND_MESSAGE)
+
+while True:
+tracer.execute()
